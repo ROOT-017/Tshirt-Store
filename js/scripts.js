3b72b4a1e1fe2bbdc4c0d734b767f7f2b255ad03
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-12-14 03:18:25 +0100

    Tshirt color element selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
-
 var products = {
     'white': {
-        
+
         'plain': {
             'unit_price': 5.12,
-            'photo': 'v-white.jpg' 
+            'photo': 'v-white.jpg'
         },
         'printed': {
             'unit_price': 8.95,
-            'photo': 'v-white-personalized.jpg' 
+            'photo': 'v-white-personalized.jpg'
         }
     },
-    
+
     'colored': {
         'plain': {
             'unit_price': 6.04,
-            'photo': 'v-color.jpg' 
+            'photo': 'v-color.jpg'
         },
         'printed': {
             'unit_price': 9.47,
-            'photo': 'v-color-personalized.png' 
+            'photo': 'v-color-personalized.png'
         }
     }
 }
@@ -41,25 +40,17 @@ var search_params = {
 // The high quality shirt (190g/m2) has a 12% increase in the unit price.
 
 // 2. Apply the following discounts for higher quantities: 
-    // 1: above 1.000 units - 20% discount
-    // 2: above 500 units - 12% discount
-    // 3: above 100 units - 5% discount
+// 1: above 1.000 units - 20% discount
+// 2: above 500 units - 12% discount
+// 3: above 100 units - 5% discount
 
 
 // Solution:
 
-$(function(){
+$(function() {
 
-  
-    
+    $("#white").click(function() {
+
+    })
+
 });
-
-
-
-
-
-
-
-
-
-
