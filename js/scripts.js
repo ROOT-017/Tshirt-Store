8abd2f7d958411a49ac02627a1b9ba90c7c1b601
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-12-18 01:20:38 +0100

    Live update

--- js/scripts.js
+++ js/scripts.js
@@ -58,17 +58,114 @@ $(function() {
         search_params.color = $("#color .option-button.selected").attr("id")
         search_params.quality = $("#quality .option-button.selected").attr("id")
         search_params.style = $("#style").val()
-        console.log(search_params)
+        console.log(search_params.quality)
     };
     updated_search_params()
 
     function Dispalay_Data() {
-        $("#result-style").html(search_params.style),
-            $("#result-quality").html(search_params.quality),
-            $("#result-color").html(search_params.color),
-            $("#result-quantity").html(search_params.quantity)
+        var styleSelector = "#style option[value=" + search_params.style + "]"
+        $("#result-style").html($(styleSelector).text())
+        var qualityid = "#" + search_params.quality
+        $("#result-quality").html($(qualityid).text())
+        var colorid = "#" + search_params.color
+        $("#result-color").html($(colorid).text())
+        $("#result-quantity").html(search_params.quantity)
+        var photoURL = "img/" + products[search_params.color][search_params.style].photo
+        console.log(photoURL)
+        $("#photo-product").attr("src", photoURL)
     }
     Dispalay_Data()
+
+
+    function calculate_price() {
+        var getQuantity = search_params.quantity
+        var unit_price = products[search_params.color][search_params.style].unit_price
+        var total_price;
+
+        if (search_params.quality == "q150") {
+            total_price = getQuantity * unit_price
+        } else if (search_params.quality == "q190") {
+            var high_unit_price = unit_price + (unit_price * 0.12)
+                // 1: above 1.000 units - 20% discount
+            if (getQuantity > 1000) {
+                high_unit_price = high_unit_price - (high_unit_price * 0.20)
+                total_price = high_unit_price * getQuantity
+            } // 2: above 500 units - 12% discount
+            else if (getQuantity > 500) {
+                high_unit_price = high_unit_price - (high_unit_price * 0.12)
+                total_price = high_unit_price * getQuantity
+            }
+            // 3: above 100 units - 5% discount
+            else if (getQuantity > 100) {
+                high_unit_price = high_unit_price - (high_unit_price * 0.05)
+                total_price = high_unit_price * getQuantity
+            }
+
+        }
+        return total_price
+    }
+    $("#total-price").html(calculate_price().toFixed(2))
+
+    $(".no-js").change(function() {
+        $("#quantity").val();
+        $("#style").val();
+        $("#color .color-option.selected").attr("id");
+        $("#quality .color-option.selected").attr("id")
+
+        function updated_search_params() {
+            search_params.quantity = $("#quantity").val()
+            search_params.color = $("#color .option-button.selected").attr("id")
+            search_params.quality = $("#quality .option-button.selected").attr("id")
+            search_params.style = $("#style").val()
+            console.log(search_params.quality)
+        };
+        updated_search_params()
+
+        function Dispalay_Data() {
+            var styleSelector = "#style option[value=" + search_params.style + "]"
+            $("#result-style").html($(styleSelector).text())
+            var qualityid = "#" + search_params.quality
+            $("#result-quality").html($(qualityid).text())
+            var colorid = "#" + search_params.color
+            $("#result-color").html($(colorid).text())
+            $("#result-quantity").html(search_params.quantity)
+            var photoURL = "img/" + products[search_params.color][search_params.style].photo
+            console.log(photoURL)
+            $("#photo-product").attr("src", photoURL)
+        }
+        Dispalay_Data()
+
+
+        function calculate_price() {
+            var getQuantity = search_params.quantity
+            var unit_price = products[search_params.color][search_params.style].unit_price
+            var total_price;
+
+            if (search_params.quality == "q150") {
+                total_price = getQuantity * unit_price
+            } else if (search_params.quality == "q190") {
+                var high_unit_price = unit_price + (unit_price * 0.12)
+                    // 1: above 1.000 units - 20% discount
+                if (getQuantity > 1000) {
+                    high_unit_price = high_unit_price - (high_unit_price * 0.20)
+                    total_price = high_unit_price * getQuantity
+                } // 2: above 500 units - 12% discount
+                else if (getQuantity > 500) {
+                    high_unit_price = high_unit_price - (high_unit_price * 0.12)
+                    total_price = high_unit_price * getQuantity
+                }
+                // 3: above 100 units - 5% discount
+                else if (getQuantity > 100) {
+                    high_unit_price = high_unit_price - (high_unit_price * 0.05)
+                    total_price = high_unit_price * getQuantity
+                }
+
+            }
+            return total_price
+        }
+        $("#total-price").html(calculate_price().toFixed(2))
+
+    })
 });
 
 
